bf6e1c3671e68b4589bbeee0350aa24ab619f092
richard-nii <richardjnr10@gmail.com>
2026-03-20 14:43:28 +0000

    connection to backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,11 +3,13 @@ const BASE_URL = "http://localhost:5000/api";
 // GET all artists
 export const getArtists = async () => {
   const res = await fetch(`${BASE_URL}/artists`);
+  if (!res.ok) throw new Error("Failed to fetch artists");
   return res.json();
 };
 
 // GET single artist
 export const getArtistById = async (id) => {
   const res = await fetch(`${BASE_URL}/artists/${id}`);
+  if (!res.ok) throw new Error("Artist not found");
   return res.json();
 };
